a1a491fa4128f6e319d903aca0ecf16c60541b7d
Sgym <daichi.sugiyama.public@gmail.com>
2023-01-31 16:21:59 +0900

    Fix comment

--- config-sample.php
+++ config-sample.php
@@ -1,13 +1,26 @@
 <?php
-#全般的な設定
-#====================================================
-#ディスク使用率の上限
+#*******************************#
+# 全般的な設定                   #
+#*******************************#
+#ディスク使用率の閾値
 define('LIMIT_DISK_USED_RATE', 80);
 #送信方法
 define('SEND_METHODS', ['mail_warn', 'main_debug', 'slack_warn', 'slack_debug']);
 
-#Slackの設定
-#====================================================
+#*******************************#
+# メールの設定                   #
+#*******************************#
+#送信先
+define('MAIL_TO', 'xxxxxxxxxxx@xxxxxxx.com');
+#送信元
+define('MAIL_FROM', 'xxxxxxxxxxx@xxxxxxx.com');
+#件名
+define('MAIL_WARN_SUBJECT', '');
+define('MAIL_DEBUG_SUBJECT', '');
+
+#*******************************#
+# Slackの設定                   #
+#*******************************#
 #通知URL
 define('SLACK_URL', '');
 #チャンネル名
@@ -17,13 +30,3 @@ define('SLACK_DEBUG_CHANNEL', '');
 define('SLACK_BOT_NAME', '');
 #メンバーID
 define('SLACK_MEMBER_ID', '<@xxxxxxx> <@xxxxxxx>');
-
-#メールの設定
-#====================================================
-#送信先
-define('MAIL_TO', 'xxxxxxxxxxx@xxxxxxx.com');
-#送信元
-define('MAIL_FROM', 'xxxxxxxxxxx@xxxxxxx.com');
-#件名
-define('MAIL_WARN_SUBJECT', '');
-define('MAIL_DEBUG_SUBJECT', '');
