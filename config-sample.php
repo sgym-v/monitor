0da89caca2ea7a46c1d8d15d20a09c0ae542c4c7
Sgym <daichi.sugiyama.public@gmail.com>
2023-01-31 14:35:29 +0900

    Add config sample

---
+++ config-sample.php
@@ -0,0 +1,17 @@
+<?php
+#Slackの通知URL
+define("SLACK_URL", "");
+
+#Slackチャンネル名
+define("SLACK_WARN_CHANNEL", "");
+define("SLACK_DEBUG_CHANNEL", "");
+
+#Slackボット名
+define('SLACK_BOT_NAME', "");
+
+#SlackのメンバーID
+define("SLACK_MEMBER_ID", "<@xxxxxxx> <@xxxxxxx>");
+
+#ディスク使用率の上限
+define("LIMIT_DISK_USED_RATE", 80);
+
